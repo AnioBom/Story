5cb43df56a3e3adb91ef58f2720da6745392c5c3
Albina <olifirenko.albina@gmail.com>
2023-04-13 12:44:43 +0300

    Initial Commit

---
+++ Story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Story
+//
+//  Created by mac on 4/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
